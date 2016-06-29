699dbe8fb6b076c9ea0a6f6896c6b445e144af81
joan.tolos <joan.tolos@thomsonreuters.com>
2016-06-29 16:59:44 +0200

    Changing the environment variables

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var config = require('./public/conf/'+process.env.TTF_LANDING_CONF+'/config.json')
+var config = require('./public/conf/'+process.env.DT_FRONTEND_LANDING+'/config.json')
 
-app.set('port', (process.env.TTF_LANDING_PORT || process.env.PORT));
+app.set('port', (process.env.DT_FRONTEND_LANDING_PORT || process.env.PORT));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
